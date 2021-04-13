61a9f92cc4bec0f8a42115f52f85deff9177d446
SamuelHosovsky <s.hosovsky@gmail.com>
2021-04-13 21:25:37 +0100

    Change the MySQL data model to select brain areas startups work on

--- be_3_9_1/BrainEmulationWebapp/BrainEmulationWebapp/settings.py
+++ be_3_9_1/BrainEmulationWebapp/BrainEmulationWebapp/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
 	'django_countries',
+    'multiselectfield',
 
 	'apps.favorites',
 	'apps.core',
--- be_3_9_1/BrainEmulationWebapp/apps/orgs/models.py
+++ be_3_9_1/BrainEmulationWebapp/apps/orgs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django_countries.fields import CountryField
+from multiselectfield import MultiSelectField
 
 
 class Category(models.Model):
@@ -16,16 +17,39 @@ class Category(models.Model):
 
 
 class Org(models.Model):
+	BRAIN_AREAS=[
+		('frontal', 'Frontal lobe'),
+		('occipit', 'Occipital lobe'),
+		('cereb', 'Cerebellum'),
+		('temp', 'Temporal lobe'),
+		('pariet', 'Parietal lobe'),
+		('corpus', 'Corpus Callosum'),
+		('pitua', 'Pituitary gland'),
+		('stem', 'Brainstem'),
+		('periph', 'Peripheral nervous system'),
+	]
+
+	#BRAIN_FUNCTIONS=[sdjasdkja]
+
+	
 	category = models.ForeignKey(Category, related_name='orgs', on_delete=models.CASCADE)
 	
 	title = models.CharField(max_length=255)
 	slug = models.SlugField(max_length=255)
+	what_and_how= models.TextField(max_length=140)
 	description = models.TextField(blank = True, null = True)
 
+	concerned_with_brain_area = MultiSelectField(
+        choices=BRAIN_AREAS,
+        max_choices=len(BRAIN_AREAS),
+		default="n/a"
+    )
+
+
 	website = models.CharField(blank = True, null = True, max_length=255)
-	team_size = models.IntegerField(blank = True, null = True)
+	team_size = models.IntegerField(null = True)
 	country_hq = CountryField(blank_label = '(select headquarters)')
-	locations = CountryField(blank_label = '(select office locations)', multiple=True)
+	all_locations = CountryField(blank_label = '(select office locations)', multiple=True)
 	
 	is_featured = models.BooleanField(default=False)
 	date_added = models.DateTimeField(auto_now_add=True)
